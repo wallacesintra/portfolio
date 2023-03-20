3cbda59bfb08b46f9d798a06d3535547626147f1
wallacelliot <103658056+wallacesintra@users.noreply.github.com>
2023-03-20 17:34:21 +0300

    update1

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// let text = "I am Wallace, the developer.";
+// let i = 0;
+// const timer = 50;
+
+// function typerWriter(){
+//     let textEffect = document.querySelector('.textEffect');
+//     if(i<text.length){
+//         textEffect.innerHTML += text.charAt(i);
+//         i++;
+//         setTimeout(typerWriter,timer);
+//     }
+// }
+// window.onload = typerWriter;
